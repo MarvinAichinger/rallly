38127e81739b47ee68ca58e7f3b04ee29405b7c9
MarvinAichinger <marvin.aichinger@outlook.com>
2026-06-26 16:34:28 +0200

    use new env var to set event scheduling

--- apps/web/src/features/scheduled-event/constants.ts
+++ apps/web/src/features/scheduled-event/constants.ts
@@ -1,4 +1,5 @@
 export const isScheduledEventEnabled =
-  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
+  process.env.NODE_ENV === "development" ||
+  process.env.SCHEDULED_EVENTS_ENABLED === "true";
 
 export const scheduledEventTag = (id: string) => `scheduled-event:${id}`;
